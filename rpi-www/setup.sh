6596b3dbf6e562ca06a8ece3e6d1f476f20646e0
Alan Raff <alanr917@hotmail.com>
2021-01-19 13:18:05 -0500

    add blank index page to prevent directory listing

--- rpi-www/setup.sh
+++ rpi-www/setup.sh
@@ -4,6 +4,7 @@ apt update
 apt install apache2 php7.3 libapache2-mod-php7.3
 pip3 install -r ~/OpenTreat/src/python/requirements.txt
 rm -rf /var/www/html/index.html
+touch /var/www/html/index.html
 cp ~/OpenTreat/src/rpi-www/rotate_auger.py /var/www/html/
 cp ~/OpenTreat/src/rpi-www/spin.php /var/www/html/
 adduser www-data gpio
