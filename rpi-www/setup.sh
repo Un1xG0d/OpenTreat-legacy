3fdef945d7122a9bfa295b023b2b7b98689eedbb
Alan Raff <alanr917@hotmail.com>
2021-01-19 12:58:42 -0500

    use absolute paths

--- rpi-www/setup.sh
+++ rpi-www/setup.sh
@@ -4,8 +4,8 @@ apt update
 apt install apache2 php7.3 libapache2-mod-php7.3
 pip3 install -r ~/OpenTreat/src/python/requirements.txt
 rm -rf /var/www/html/index.html
-cp rotate_auger.py /var/www/html/
-cp spin.php /var/www/html/
+cp ~/OpenTreat/src/rpi-www/rotate_auger.py /var/www/html/
+cp ~/OpenTreat/src/rpi-www/spin.php /var/www/html/
 adduser www-data gpio
 chown root.gpio /dev/mem
 chmod g+rw /dev/mem
