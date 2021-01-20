e3a08a214abf431f83940e80b33e21c93ad5aa60
Alan Raff <alanr917@hotmail.com>
2021-01-19 19:01:44 -0500

    reduce time before PHP page automatically closes

--- rpi-www/spin.php
+++ rpi-www/spin.php
@@ -8,8 +8,8 @@
 		function sleep (time) {
 		  return new Promise((resolve) => setTimeout(resolve, time));
 		}
-		//sleep for 5 seconds after the rotate_auger.py script is done executing then close the current popup window
-		sleep(5000).then(() => {
+		//sleep for 2.5 seconds after the rotate_auger.py script is done executing then close the current popup window
+		sleep(2500).then(() => {
 			window.close()
 		});
 		</script>
