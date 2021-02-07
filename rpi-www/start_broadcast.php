4bd63a88927f6b0ba41c5427715cefa57df34d89
Alan Raff <alanr917@hotmail.com>
2021-02-07 05:36:41 -0500

    automatically close window after script completes

--- rpi-www/start_broadcast.php
+++ rpi-www/start_broadcast.php
@@ -1,3 +1,21 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<script>
+		//sleep time expects milliseconds
+		function sleep (time) {
+		  return new Promise((resolve) => setTimeout(resolve, time));
+		}
+		//sleep after the rotate_auger.py script is done executing then close the current popup window
+		sleep(1).then(() => {
+			window.close()
+		});
+		</script>
+	</body>
+</html>
+
 <?php
 $command = "/bin/bash /var/www/html/start_broadcast_as_pi.sh";
 exec($command, $output);
