80c1fb5920035ffc6ac38356b29b1e0d0b4004c9
Alan Raff <alanr917@hotmail.com>
2021-02-07 03:23:27 -0500

    run pip as regular user; copy needed files to correct directory

--- rpi-www/setup.sh
+++ rpi-www/setup.sh
@@ -2,13 +2,14 @@
 
 apt update
 apt install apache2 php7.3 libapache2-mod-php7.3
-sudo -H pip3 install -r ~/OpenTreat/src/python/requirements.txt
+pip3 install -r ~/OpenTreat/src/python/requirements.txt
 rm -rf /var/www/html/index.html
 touch /var/www/html/index.html
 cp ~/OpenTreat/src/rpi-www/rotate_auger.py /var/www/html/
 cp ~/OpenTreat/src/rpi-www/spin.php /var/www/html/
 cp ~/OpenTreat/src/rpi-www/restart.php /var/www/html/
 cp ~/OpenTreat/src/rpi-www/start_broadcast.php /var/www/html/
+cp ~/OpenTreat/src/rpi-www/start_broadcast_as_pi.sh /var/www/html/
 cp ~/OpenTreat/src/python/broadcast_webcam.py /var/www/html/
 adduser www-data gpio
 chown root.gpio /dev/mem
