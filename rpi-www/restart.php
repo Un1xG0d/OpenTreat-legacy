b91f2ab43c45c603797162298f341b32ffd1e610
Alan Raff <alanr917@hotmail.com>
2021-04-06 09:37:02 -0400

    clean up html and js

--- rpi-www/restart.php
+++ rpi-www/restart.php
@@ -1,21 +1,3 @@
-<html>
-<head>
-	<meta charset="UTF-8">
-</head>
-<body>
-	<script>
-		//sleep time expects milliseconds
-		function sleep (time) {
-			return new Promise((resolve) => setTimeout(resolve, time));
-		}
-		//sleep then close the current popup window
-		sleep(1).then(() => {
-			window.close()
-		});
-	</script>
-</body>
-</html>
-
 <?php
 $command = "sudo /sbin/reboot";
 exec($command, $output);
--- rpi-www/spin.php
+++ rpi-www/spin.php
@@ -1,21 +1,3 @@
-<html>
-	<head>
-		<meta charset="UTF-8">
-	</head>
-	<body>
-		<script>
-		//sleep time expects milliseconds
-		function sleep (time) {
-		  return new Promise((resolve) => setTimeout(resolve, time));
-		}
-		//sleep after the rotate_auger.py script is done executing then close the current popup window
-		sleep(1).then(() => {
-			window.close()
-		});
-		</script>
-	</body>
-</html>
-
 <?php
 $command = "python3 rotate_auger.py";
 exec($command, $output);
